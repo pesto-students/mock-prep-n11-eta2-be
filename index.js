65ac5e07fbbf0aaafcd6210b8864077d24287e2f
unknown <Mohammed saif>
2021-11-30 13:42:17 +0530

    Express app

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 3000 ;
+
+app.use(express.static("public"))
 
 app.get('/', (req, res) => {
   res.send('Welcome to Mock prep!')
