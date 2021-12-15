045900a8d45f446025ba1e0ad7d3caa9676da8e3
Mohammed Saif <saifmohammed888@gmail.com>
2021-12-15 16:53:55 +0530

    Deploy Heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser')
-const ad = require('Routes')
+
 // const adminRoute = require('./server/routes/admin')
 // const UserRoute = require('./server/routes/User')
 // const StudentRoute = require('./server/routes/Student')
