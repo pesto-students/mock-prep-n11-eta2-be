380d39a15d7e888572cde387f926e21cc3c25fe3
Mohammed Saif <saifmohammed888@gmail.com>
2022-01-01 03:04:05 +0530

    Code Refactor, Added Prettier and Eslint formatting

--- server/routes/quiz.js
+++ server/routes/quiz.js
@@ -5,7 +5,7 @@ const QuizController = require("../controller/quiz")
 router.get("/getData", QuizController.find)
 router.get("/getData/:id", QuizController.findOne)
 router.post("/addData", QuizController.create)
-router.post("/updataData/:id", QuizController.update)
+router.post("/updateData/:id", QuizController.update)
 router.delete("/deleteData/:id", QuizController.delete)
 
 module.exports = router 
